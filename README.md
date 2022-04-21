9f1c51e00d1ee118ed19464950a24d494d407d1a
Igor Loskutov <igor.loskutoff@gmail.com>
2022-04-21 21:32:58 +0700

    readme & repl fixes

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-TODO 
+## How to run
 
-- some REPL expectations aren't held right now - because yargs lib is used to parse args + generate help and it seems yargs is better for CLI
+- make sure you have node installed; the version it's tested with is `v16.6.2`
+- checkout the repo, and run the following commands:
+
+```
+npm install
+npm run start
+```
+
+## Improvements todo 
+
+- some REPL expectations aren't held right now - because yargs lib is used to parse args + generate help and it seems yargs is made for CLI not for REPL
+- fix help text removing binary prefix from it
 - we could easily make it "multi-storages" app
 - validate args
+- package it into a binary!
--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const r = repl.start({
         r.close();
       })
       .command(
-        "init W H",
+        "init [W] [H]",
         "(Re)Initialises the application as an empty W x H warehouse.",
         async ({ argv: { _ } }: any) => {
           await commandHandlers.init({ w: _[1], h: _[2] });
@@ -22,7 +22,7 @@ const r = repl.start({
         }
       )
       .command(
-        "store X Y W H P",
+        "store [X] [Y] [W] [H] [P]",
         "Stores a crate of product code P and of size W x H at position (X,Y).",
         async ({ argv: { _ } }: any) => {
           await commandHandlers.store({
@@ -36,14 +36,14 @@ const r = repl.start({
         }
       )
       .command(
-        "locate P",
+        "locate [P]",
         "Show a list of all locations occupied by product code P.",
         (argv) => {
           cb(null, argv);
         }
       )
       .command(
-        "remove X Y",
+        "remove [X] [Y]",
         "Remove the entire crate occupying the location (X,Y).",
         (argv) => {
           cb(null, argv);
@@ -62,15 +62,21 @@ const r = repl.start({
       .command("$0", "", () => {
         cb(null, `Unknown command "${cmd}". Use 'help' to see help.`);
       })
-      .strict(false)
       .strictCommands(false)
-      .strictOptions(false);
+      .strictOptions(false)
+      .strict(false);
+
     // .alias({'exit': 'quit'})
     if (cmd === "help") {
       cb(null, await y.getHelp());
       return;
     } else {
-      y.parse();
+      try {
+        y.parse();
+      } catch (e) {
+
+      }
+
     }
   },
   writer: modifyOutput,
